ee7684514f50420aee334861b2994f8ee54c7508
mrjouts <es.hotes@gmail.com>
2020-08-19 23:25:46 -0300

    clean pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,6 @@ import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CommentsPipe } from './pipes/comments.pipe';
 import { SearchFilterPipe } from './pipes/search-filter.pipe';
-import { GdmFormatPipe } from './pipes/gdm-format.pipe';
-import { LocalDatePipe } from './pipes/local-date.pipe';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 
@@ -33,8 +31,6 @@ import { RegistroComponent } from './components/registro/registro.component';
     FooterComponent,
     CommentsPipe,
     SearchFilterPipe,
-    GdmFormatPipe,
-    LocalDatePipe,
     LoginComponent,
     RegistroComponent
   ],
--- src/app/pipes/gdm-format.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GdmFormatPipe } from './gdm-format.pipe';
-
-describe('GdmFormatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GdmFormatPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
